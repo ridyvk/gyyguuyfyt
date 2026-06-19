4a500821186f93e625fb8e8c993e3cea1324968c
ridyvk <kamiridy@gmail.com>
2026-06-19 15:50:34 +0900

    Disable stale PWA caching during recovery

--- sw.js
+++ sw.js
@@ -1,58 +1,13 @@
-const CACHE_NAME = 'kpi-scope-pwa-v1'
-const APP_SHELL = ['/gyyguuyfyt/', '/gyyguuyfyt/index.html', '/gyyguuyfyt/manifest.webmanifest', '/gyyguuyfyt/icon.svg']
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
-  self.skipWaiting()
-})
+self.addEventListener('install', () => self.skipWaiting())
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches
-      .keys()
-      .then((keys) =>
-        Promise.all(
-          keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
-        ),
-      )
-      .then(() => self.clients.claim()),
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))).then(() => self.clients.claim())
   )
 })
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return
-
-  const requestUrl = new URL(event.request.url)
-  if (requestUrl.origin !== self.location.origin || !requestUrl.pathname.startsWith('/gyyguuyfyt/')) {
-    return
+  if (event.request.method === 'GET') {
+    event.respondWith(fetch(event.request))
   }
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          const copy = response.clone()
-          void caches.open(CACHE_NAME).then((cache) => cache.put('/gyyguuyfyt/index.html', copy))
-          return response
-        })
-        .catch(() => caches.match('/gyyguuyfyt/index.html')),
-    )
-    return
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const network = fetch(event.request)
-        .then((response) => {
-          if (response.ok) {
-            const copy = response.clone()
-            void caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
-          }
-          return response
-        })
-        .catch(() => cached)
-
-      return cached ?? network
-    }),
-  )
 })
