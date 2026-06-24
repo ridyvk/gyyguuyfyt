c6b0f362f595becc16837b09692d5073b2c791d2
ridyvk <kamiridy@gmail.com>
2026-06-24 11:35:41 +0900

    fix: align source service worker data caching

--- sw.js
+++ sw.js
@@ -1,15 +1,72 @@
-const KPI_SCOPE_SW_VERSION = '20260619-photo1'
+const CACHE_NAME = 'kpi-scope-v4-nodata'
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180-photo1.png',
+  './icon-192-photo1.png',
+  './icon-512-photo1.png',
+  './icon-maskable-192-photo1.png',
+  './icon-maskable-512-photo1.png',
+]
 
-self.addEventListener('install', () => self.skipWaiting())
+const isSameOriginDataRequest = (request) => {
+  const url = new URL(request.url)
+  return url.origin === self.location.origin && url.pathname.includes('/data/')
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
+  self.skipWaiting()
+})
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
+        ),
+      ),
   )
+  self.clients.claim()
 })
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method === 'GET') {
+  if (event.request.method !== 'GET') return
+
+  if (isSameOriginDataRequest(event.request)) {
     event.respondWith(fetch(event.request))
+    return
   }
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone()
+          void caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy))
+          return response
+        })
+        .catch(() => caches.match('./index.html')),
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+            const copy = response.clone()
+            void caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+          }
+          return response
+        })
+        .catch(() => cached)
+
+      return cached ?? network
+    }),
+  )
 })
